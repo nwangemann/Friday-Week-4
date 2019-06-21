6f2ec1ad3f6a0c03707784d6db791aaf1ca76988
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-21 10:11:10 -0700

    add form and inputs for the order selections

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // Business Logic
 
+// Constructor for determining how orders will be created
 function Order(name, size, crust, toppings, cost) {
   this.name = name;
   this.size = size;
@@ -8,6 +9,7 @@ function Order(name, size, crust, toppings, cost) {
   this.cost = cost;
 }
 
+//prototype to take values stored from order properties and return total price value
 Order.prototype.priceCalc = function() {
   return this.cost = parseInt(this.size) + parseInt(this.crust) + parseInt(this.toppings)
 }
@@ -21,6 +23,8 @@ Order.prototype.priceCalc = function() {
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    var userName = $("#nameInput").val();
+    console.log(userName);
     var testOrder = new Order("TestPerson", "2", "3", "8", "0")
     console.log(testOrder);
     var testComplete = testOrder.priceCalc();
